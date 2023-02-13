f588209e85a98a2e1fd49a957bc2c98d2353ed21
unknown <bryangabrielberja25@gmail.com>
2023-02-13 12:18:39 +0800

    Update index,php

--- index.php
+++ index.php
@@ -68,12 +68,12 @@ if($_SESSION['status'] == 'invalid' || empty($_SESSION['status'])){
                             <div class="sb-sidenav-menu-heading">Interface</div>
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Layouts
+                                User Management
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
                             <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="layout-static.php">Static Navigation</a>
+                                    <a class="nav-link" href="pages/member/member.php">Member</a>
                                     <a class="nav-link" href="layout-sidenav-light.php">Light Sidenav</a>
                                 </nav>
                             </div>
